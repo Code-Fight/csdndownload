284ac0499683f10f870f9a3fdab9b4934595daff
97576419@qq.com <tingting2013>
2016-01-04 22:15:06 +0800

    201614

---
+++ CsdnDownload/DAL/IPCollectionDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FTools.SQL;
+using FTools.SQL.SqLite;
+
+namespace DAL
+{
+    public class IPCollectionDal : SQLiteHelper
+    {
+        public void AddTest()
+        {
+            const string str = "insert into t_ip_acquisition_rule (rule_name,rule_url,rule_rex)values('测试name','测试url','测试rex')";
+            Execute(str);
+        }
+    }
+}
