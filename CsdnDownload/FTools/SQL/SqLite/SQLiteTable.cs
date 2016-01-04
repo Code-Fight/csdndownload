5f2d907753d8652c46a87cf9f77517f8aaafdb89
97576419@qq.com <tingting2013>
2016-01-04 22:17:55 +0800

    sql

---
+++ CsdnDownload/FTools/SQL/SqLite/SQLiteTable.cs
@@ -0,0 +1,16 @@
+﻿namespace FTools.SQL.SqLite
+{
+    public class SQLiteTable
+    {
+        public string TableName = "";
+        public SQLiteColumnList Columns = new SQLiteColumnList();
+
+        public SQLiteTable()
+        { }
+
+        public SQLiteTable(string name)
+        {
+            TableName = name;
+        }
+    }
+}
