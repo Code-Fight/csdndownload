2995d3ddd33b8524aa0df0cbe1165ef58ff17fe9
97576419@qq.com <tingting2013>
2016-01-10 21:26:07 +0800

    20160110

---
+++ CsdnDownload/Bll/IP/IPCollectionBll.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+using DAL;
+using Entity;
+
+namespace Bll.IP
+{
+    public class IpCollectionBll
+    {
+
+        #region DBA 
+
+        public string GetIp()
+        {
+            return string.Empty;
+        }
+
+
+        public void Add(IpCollectionEntity info)
+        {
+            IpCollectionDal dal = new IpCollectionDal();
+            dal.Add(info);
+        }
+
+        public DataTable GetAll()
+        {
+            IpCollectionDal dal = new IpCollectionDal();
+            return dal.GetAll();
+        }
+
+        public void Update(IpCollectionEntity info)
+        {
+            IpCollectionDal dal = new IpCollectionDal();
+            dal.Update(info);
+        }
+
+        public void Delete(int id)
+        {
+            IpCollectionDal dal = new IpCollectionDal();
+            dal.Delete(id);
+        }
+
+        #endregion
+
+    }
+}
---
+++ CsdnDownload/Bll/IP/IPCollectionHttpBll.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FTools.HTTP;
+
+namespace Bll.IP
+{
+    public class IpCollectionHttpBll
+    {
+
+
+        //TODO:写采集
+
+
+
+
+        /// <summary>
+        /// 访问网页
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public string GetHtml(string url)
+        {
+            System.Net.CookieContainer cc = new System.Net.CookieContainer();//自动处理Cookie对象
+            HttpHelpers helper = new HttpHelpers();//发起请求对象
+            HttpItems items = new HttpItems();//请求设置对象
+            HttpResults hr = new HttpResults();//请求结果
+            items.URL = url;//设置请求地址
+            items.Container = cc;//自动处理Cookie时,每次提交时对cc赋值即可
+            hr = helper.GetHtml(items);//发起请求并得到结果
+            return hr.Html;//得到请求结果
+        }
+    }
+}
